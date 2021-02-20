5df5b1b6e954cec38ee7702fc706db01ab343452
Chris Taylor <christay@usc.edu>
2021-02-20 14:35:32 -0800

    del kgi ping lmu

--- Scripts_and_Tools/make-summaries/make-summary1.py
+++ Scripts_and_Tools/make-summaries/make-summary1.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3 -u
 import subprocess
+import numpy
 import re
 INST = ['Caltech',  'Claremont-HMC',  'KGI',  'LMU',  'Oxy',  'Pomona',  'USC']
 PATH = '../../Results-and-Metrics/'
 
+def average(inp_lst):
+  return round(numpy.average(inp_lst), 2)
+
 def printping():
+  p = re.compile(' time=([0-9]*\.[0-9]*) ms')
+
   for i in INST:
     for ii in INST:
+      inp_lst = []
+      print('ping from ' + i + ' to ' + ii + ':')
       filepath = PATH + i + '/ping_to_' + ii + '.txt'
-      print(filepath)
       try:
-        ping = open(filepath + '/ping_to_' + ii + '.txt', 'r')
+        ping = open(filepath, 'r')
       except:
         ping = None
+      if ping:
+        pingraw = ping.readlines()
+        for line in pingraw:
+          pingtime = p.search(line)
+          if pingtime:
+            inp_lst.append(float(pingtime.group(1)))
+        print(str(average(inp_lst)) + ' ms average')
+        print('')
       else:
-        print('here')
-
-  if ping:
-    print(ping.read())
+        print('Not available.')
 
 def main():
   printping()
